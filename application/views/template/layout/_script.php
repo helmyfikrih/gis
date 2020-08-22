08d953c3c0e46ada6725c9cfa4d4824a098cd24f
Helmy Fikri Husein <helmyfikrih@gmail.com>
2020-08-22 18:53:26 +0700

    init commit & templating

---
+++ application/views/template/layout/_script.php
@@ -0,0 +1,19 @@
+<!-- jQuery -->
+<script src="<?= base_url() ?>assets/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?= base_url() ?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?= base_url() ?>assets/dist/js/adminlte.min.js"></script>
+
+<?php
+foreach ($plugins_path_js as $plugin_js) { ?>
+    <script src="<?= base_url() ?>assets/plugins/<?= $plugin_js ?>"></script>
+<?php
+}
+?>
+<?php
+foreach ($js_path as $js) { ?>
+    <script src="<?= base_url() ?>assets/dist/js/<?= $js ?>"></script>
+<?php
+}
+?>
---
+++ application/views/template/layout/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>AdminLTE 3 | Starter</title>
+
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/dist/css/adminlte.min.css">
+    <?php
+    foreach ($plugins_path_css as $plugin_css) { ?>
+        <link href="<?= base_url() ?>assets/plugins/<?= $plugin_css ?>" rel="stylesheet">
+    <?php
+    }
+    ?>
+    <?php
+    foreach ($css_path as $css) { ?>
+        <link href="<?= base_url() ?>assets/dist/css/<?= $css ?>" rel="stylesheet">
+    <?php
+    }
+    ?>
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
