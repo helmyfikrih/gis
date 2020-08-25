58e3d472f22213727aa32e3d580d01c060f279c9
Helmy Fikri Husein <helmyfikrih@gmail.com>
2020-08-25 20:13:21 +0700

    alt name logo header

--- application/views/template/layout/_sidebar.php
+++ application/views/template/layout/_sidebar.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?= base_url() ?>" class="brand-link">
-        <img src="<?= base_url() ?>assets/dist/img/<?= $setting->system_settings_app_logo_header ?>?x=<?= time() ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="<?= base_url() ?>assets/dist/img/<?= $setting->system_settings_app_logo_header ?>?x=<?= time() ?>" alt="<?= $setting->system_settings_app_name ?>" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light"><?= $setting->system_settings_app_name ?></span>
     </a>
 
