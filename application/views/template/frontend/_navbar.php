338ebaa7667ef7f45491a0919db87093d172cca0
Helmy Fikri Husein <helmyfikrih@gmail.com>
2020-08-26 14:06:13 +0700

    adding gis page

---
+++ application/views/template/frontend/_navbar.php
@@ -0,0 +1,12 @@
+   <!-- menu start -->
+   <div class="menu_side">
+       <div id="navbar_menu">
+           <ul class="first-ul">
+               <li>
+                   <a href="<?= base_url('frontend/home') ?>">Home</a>
+               </li>
+               <li><a href="it_about.html">About Us</a></li>
+           </ul>
+       </div>
+   </div>
+   <!-- menu end -->
