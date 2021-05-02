2504cfeeaa28e89ad0247a179e8dcb749312c5fc
Helmy Fikri Husein <helmyfikrih@gmail.com>
2021-05-02 18:17:34 +0700

    WIP: Filter

--- application/controllers/frontend/Map.php
+++ application/controllers/frontend/Map.php
@@ -3,33 +3,59 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Map extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-        $this->plugins_path_css = array();
-        $this->plugins_path_js = array();
-        $this->css_path = array();
-        $this->js_path = array();
-    }
+	public function __construct()
+	{
+		parent::__construct();
+		$this->plugins_path_css = array();
+		$this->plugins_path_js = array();
+		$this->css_path = array();
+		$this->js_path = array();
+	}
 
-    public function index()
-    {
-        $this->plugins_path_css = array(
-            'select2/css/select2.min.css',
-        );
-        $this->plugins_path_js = array(
-            'select2/js/select2.full.min.js',
-        );
-        $this->js_path = array(
-            'pages/map.js',
-        );
-        $data = array(
-            'header_title' => "Home",
-            'css_path' => $this->css_path,
-            'plugins_path_css' => $this->plugins_path_css,
-            'plugins_path_js' => $this->plugins_path_js,
-            'js_path' => $this->js_path,
-        );
-        $this->template->load('frontend', 'frontend/map', $data);
-    }
+	public function index()
+	{
+		$this->plugins_path_css = array(
+			'select2/css/select2.min.css',
+		);
+		$this->plugins_path_js = array(
+			'select2/js/select2.full.min.js',
+		);
+		$this->js_path = array(
+			'pages/map.js',
+		);
+		$data = array(
+			'header_title' => "Home",
+			'css_path' => $this->css_path,
+			'plugins_path_css' => $this->plugins_path_css,
+			'plugins_path_js' => $this->plugins_path_js,
+			'js_path' => $this->js_path,
+			'select_kota' => getSelectKota(),
+		);
+		$this->template->load('frontend', 'frontend/map', $data);
+	}
+
+	public function filter()
+	{
+		$this->load->model('Gis_model', 'gis');
+		$f_search = trim($this->input->post('f_search'));
+		$f_kota = $this->input->post('f_kota');
+		$f_kecamatan = $this->input->post('f_kecamatan');
+		$cond = array();
+		if ($f_search) {
+			$cond["dd.developer_name LIKE '%$f_search%' OR dd.developer_address LIKE '%$f_search%'"] = null;
+		}
+		if ($f_kota && ($f_kota != '0' || $f_kota != '0')) {
+			$cond['k.kota_id'] = $f_kota;
+		}
+		if ($f_kecamatan && ($f_kecamatan != '0' || $f_kecamatan != '0')) {
+			$cond['k.kecamatan_id'] = $f_kecamatan;
+		}
+
+		$data = $this->gis->get_persebaran($cond);
+		echo json_encode(
+			array(
+				'data' => $data
+			)
+		);
+	}
 }
--- assets/frontend/js/pages/map.js
+++ assets/frontend/js/pages/map.js
@@ -1,11 +1,7 @@
 // A $( document ).ready() block.
-var locations = [
-	["Bondi Beach", -33.890542, 151.274856, 4],
-	["Coogee Beach", -33.923036, 151.259052, 5],
-	["Cronulla Beach", -34.028249, 151.157507, 3],
-	["Manly Beach", -33.80010128657071, 151.28747820854187, 2],
-	["Maroubra Beach", -33.950198, 151.259302, 1],
-];
+var user_lat = "";
+var user_lng = "";
+var locations = [["Ragunan", -6.311416722862261, 106.82029851602512]];
 $(document).ready(function () {
 	$(".select2").select2({});
 });
@@ -17,7 +13,9 @@ if (navigator.geolocation) {
 function setPosition(position) {
 	$("#curr_lat").val(position.coords.latitude);
 	$("#curr_lng").val(position.coords.longitude);
-	initMap();
+	user_lat = position.coords.latitude;
+	user_lng = position.coords.longitude;
+	initMap(user_lat, user_lng);
 }
 
 function getDirection(start, end) {
@@ -29,8 +27,8 @@ function getDirection(start, end) {
 	map = new google.maps.Map(document.getElementById("map"), myOptions);
 	directionsDisplay.setMap(map);
 	var directionsService = new google.maps.DirectionsService();
-	var start = `${$("#curr_lat").val()}, ${$("#curr_lng").val()}`;
-	var end = "-6.2008406, 106.7987143";
+	// var start = `${$("#curr_lat").val()}, ${$("#curr_lng").val()}`;
+	// var end = "-6.2008406, 106.7987143";
 	var request = {
 		origin: start,
 		destination: end,
@@ -54,34 +52,84 @@ function getDirection(start, end) {
 	});
 }
 
-function initMap() {
+function initMap(user_lat = null, user_lng = null, data = null) {
 	var map = new google.maps.Map(document.getElementById("map"), {
 		zoom: 10,
-		center: new google.maps.LatLng(-33.92, 151.25),
+		center: new google.maps.LatLng(user_lat, user_lng),
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
 	});
 
+	var user_marker = new google.maps.MarkerImage(
+		`${base_url}assets/frontend/images/it_service/user_position.png`,
+		null /* size is determined at runtime */,
+		null /* origin is 0,0 */,
+		null /* anchor is bottom center of the scaled image */,
+		new google.maps.Size(40, 40)
+	);
+	marker = new google.maps.Marker({
+		position: new google.maps.LatLng(user_lat, user_lng),
+		map: map,
+		icon: user_marker,
+	});
+	google.maps.event.addListener(
+		marker,
+		"click",
+		(function (marker, i) {
+			return function () {
+				infowindow.setContent("Your Current Position");
+				infowindow.open(map, marker);
+			};
+		})(marker, i)
+	);
 	var infowindow = new google.maps.InfoWindow();
 
 	var marker, i;
-	var image = `${base_url}assets/frontend/images/it_service/location_icon_map_cont.png`;
-	for (i = 0; i < locations.length; i++) {
-		marker = new google.maps.Marker({
-			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-			map: map,
-			icon: image,
-		});
+	var image_url = `${base_url}assets/frontend/images/it_service/location_icon_map_cont.png`;
+	var place_marker = new google.maps.MarkerImage(
+		image_url,
+		null /* size is determined at runtime */,
+		null /* origin is 0,0 */,
+		null /* anchor is bottom center of the scaled image */,
+		new google.maps.Size(40, 40)
+	);
+	console.log(data);
+	var user_position = new google.maps.LatLng(user_lat, user_lng);
+	if (data) {
+		$.each(data, function (key, value) {
+			marker = new google.maps.Marker({
+				position: new google.maps.LatLng(
+					value.developer_lat,
+					value.developer_lng
+				),
+				map: map,
+				icon: place_marker,
+			});
 
-		google.maps.event.addListener(
-			marker,
-			"click",
-			(function (marker, i) {
-				return function () {
-					infowindow.setContent(locations[i][0]);
-					infowindow.open(map, marker);
-				};
-			})(marker, i)
-		);
+			var target_position = new google.maps.LatLng(
+				value.developer_lat,
+				value.developer_lng
+			);
+			var distance = getDistance(user_position, target_position);
+			var btn_direction = `<a href="javascript:;" onclick="getDirection('${
+				user_lat + "," + user_lng
+			}','${
+				value.developer_lat + "," + value.developer_lng
+			}')"><i class="fa fa-search" aria-hidden="true"></i> Get Direction</a>`;
+			var info_window_contnet = ` Location Name: ${value.developer_name} 
+		<br> Distance: ${Math.round(distance)} M From Your Location
+		<br> ${btn_direction}
+		`;
+			google.maps.event.addListener(
+				marker,
+				"click",
+				(function (marker, i) {
+					return function () {
+						infowindow.setContent(info_window_contnet);
+						infowindow.open(map, marker);
+					};
+				})(marker, i)
+			);
+		});
 	}
 	// var image = "images/it_service/location_icon_map_cont.png";
 
@@ -90,4 +138,89 @@ function initMap() {
 	//   map: map,
 	//   icon: image,
 	// });
+}
+
+function getKecamatan(kota) {
+	$("#f_kecamatan option").remove();
+	$.ajax({
+		url: `${base_url}gis/getKecamatan`,
+		type: "POST",
+		data: {
+			kota: kota,
+		},
+		dataType: "json",
+		success: function (data) {
+			$("#f_kecamatan").append(`<option value="0">ALL</option>`);
+			$.each(data, function (key, value) {
+				$("#f_kecamatan").append(
+					`<option value="${value.kecamatan_id}">${value.kecamatan_name}</option>`
+				);
+			});
+		},
+		error: function (xhr, status, error) {
+			Swal.fire({
+				title: error,
+				// text: response.message,
+				icon: "error",
+			});
+		},
+		timeout: 300000, // sets timeout to 5 minutes
+	});
+}
+
+// change handler
+$("#f_kota").on("change", function (e) {
+	// Do something
+	getKecamatan($("#f_kota").val());
+});
+
+$(".select2").on("change", function (e) {
+	// Do something
+	filter_persebaran();
+});
+
+var rad = function (x) {
+	return (x * Math.PI) / 180;
+};
+
+var getDistance = function (p1, p2) {
+	var R = 6378137; // Earth’s mean radius in meter
+	var dLat = rad(p2.lat() - p1.lat());
+	var dLong = rad(p2.lng() - p1.lng());
+	var a =
+		Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+		Math.cos(rad(p1.lat())) *
+			Math.cos(rad(p2.lat())) *
+			Math.sin(dLong / 2) *
+			Math.sin(dLong / 2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	var d = R * c;
+	return d; // returns the distance in meter
+};
+
+function filter_persebaran() {
+	var f_search = $("#f_search").val();
+	var f_kota = $("#f_kota").val();
+	var f_kecamatan = $("#f_kecamatan").val();
+	$.ajax({
+		url: `${base_url}frontend/map/filter`,
+		type: "POST",
+		data: {
+			f_search: f_search,
+			f_kota: f_kota,
+			f_kecamatan: f_kecamatan,
+		},
+		dataType: "json",
+		success: function (result) {
+			initMap(user_lat, user_lng, result.data);
+		},
+		error: function (xhr, status, error) {
+			Swal.fire({
+				title: error,
+				// text: response.message,
+				icon: "error",
+			});
+		},
+		timeout: 300000, // sets timeout to 5 minutes
+	});
 }
